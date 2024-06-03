fce6fbdb569fe8e7e9919984fb644373bdafb381
Emanoel Viana <emviana@uolinc.com>
2024-06-03 09:48:44 -0300

    Bug fix on Navigation Bar component.

--- lib/src/core/widgets/desktop/app_bar/dsk_navigation_bar.dart
+++ lib/src/core/widgets/desktop/app_bar/dsk_navigation_bar.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-
 import 'package:remixicon/remixicon.dart';
-
 import 'package:emanoelviana/src/core/extensions/theme_extension.dart';
 import 'package:emanoelviana/src/core/utils/responsive_screen_provider.dart';
 
@@ -13,41 +11,53 @@ class DskNavigationBar extends StatelessWidget implements PreferredSizeWidget {
     return AppBar(
       toolbarHeight: 80,
       titleSpacing: ResponsiveScreenProvider.isDesktopDevice(context) ? 48 : 16,
-      title: Expanded(
-        child: LayoutBuilder(builder: (context, constraints) {
-          return Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      title: Stack(alignment: Alignment.center, children: [
+        Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Text('Emanoel Viana', style: context.text.titleMedium),
+          Wrap(
+              crossAxisAlignment: WrapCrossAlignment.center,
+              direction: Axis.horizontal,
+              spacing: 16,
               children: [
-                Text('Emanoel Viana', style: context.text.titleMedium),
-                Wrap(
-                    direction: Axis.horizontal,
-                    crossAxisAlignment: WrapCrossAlignment.center,
-                    spacing: 16,
-                    children: [
-                      TextButton(
-                          onPressed: () {}, child: const Text('Projects')),
-                      TextButton(onPressed: () {}, child: const Text('About')),
-                      TextButton(onPressed: () {}, child: const Text('Blog')),
-                    ]),
-                Wrap(
-                    crossAxisAlignment: WrapCrossAlignment.center,
-                    direction: Axis.horizontal,
-                    spacing: 16,
-                    children: [
-                      IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Remix.github_fill)),
-                      IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Remix.medium_fill)),
-                      IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Remix.behance_fill)),
-                    ]),
-              ]);
-        }),
-      ),
+                IconButton(
+                    onPressed: () {}, icon: const Icon(Remix.github_fill)),
+                IconButton(
+                    onPressed: () {}, icon: const Icon(Remix.medium_fill)),
+                IconButton(
+                    onPressed: () {}, icon: const Icon(Remix.behance_fill)),
+              ]),
+        ]),
+        Wrap(
+            direction: Axis.horizontal,
+            crossAxisAlignment: WrapCrossAlignment.center,
+            spacing: 4,
+            children: [
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                    foregroundColor: context.color.onSurface,
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 16, horizontal: 24)),
+                child: const Text('Projects'),
+              ),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                    foregroundColor: context.color.onSurface,
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 16, horizontal: 24)),
+                child: const Text('About'),
+              ),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                    foregroundColor: context.color.onSurface,
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 16, horizontal: 24)),
+                child: const Text('Blog'),
+              ),
+            ]),
+      ]),
     );
   }
 
