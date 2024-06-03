1f1953e5d94d05927949ca7a1650dd4f26c01603
Emanoel Viana <emviana@uolinc.com>
2024-06-03 03:04:34 -0300

    Correction: Removing Stack from the title of App Bar. Now testing a Row

--- lib/src/core/widgets/desktop/app_bar/dsk_navigation_bar.dart
+++ lib/src/core/widgets/desktop/app_bar/dsk_navigation_bar.dart
@@ -5,8 +5,7 @@ import 'package:remixicon/remixicon.dart';
 import 'package:emanoelviana/src/core/extensions/theme_extension.dart';
 import 'package:emanoelviana/src/core/utils/responsive_screen_provider.dart';
 
-class DskNavigationBar extends StatelessWidget
-    implements PreferredSizeWidget {
+class DskNavigationBar extends StatelessWidget implements PreferredSizeWidget {
   const DskNavigationBar({super.key});
 
   @override
@@ -16,32 +15,37 @@ class DskNavigationBar extends StatelessWidget
       titleSpacing: ResponsiveScreenProvider.isDesktopDevice(context) ? 48 : 16,
       title: Expanded(
         child: LayoutBuilder(builder: (context, constraints) {
-          return Stack(alignment: Alignment.center, children: [
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-              Text('Emanoel Viana', style: context.text.titleLarge),
-              Wrap(
-                  crossAxisAlignment: WrapCrossAlignment.center,
-                  direction: Axis.horizontal,
-                  spacing: 16,
-                  children: [
-                    IconButton(
-                        onPressed: () {}, icon: const Icon(Remix.github_fill)),
-                    IconButton(
-                        onPressed: () {}, icon: const Icon(Remix.medium_fill)),
-                    IconButton(
-                        onPressed: () {}, icon: const Icon(Remix.behance_fill)),
-                  ]),
-            ]),
-            Wrap(
-                direction: Axis.horizontal,
-                crossAxisAlignment: WrapCrossAlignment.center,
-                spacing: 16,
-                children: [
-                  TextButton(onPressed: () {}, child: const Text('Projects')),
-                  TextButton(onPressed: () {}, child: const Text('About')),
-                  TextButton(onPressed: () {}, child: const Text('Blog')),
-                ]),
-          ]);
+          return Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Emanoel Viana', style: context.text.titleMedium),
+                Wrap(
+                    direction: Axis.horizontal,
+                    crossAxisAlignment: WrapCrossAlignment.center,
+                    spacing: 16,
+                    children: [
+                      TextButton(
+                          onPressed: () {}, child: const Text('Projects')),
+                      TextButton(onPressed: () {}, child: const Text('About')),
+                      TextButton(onPressed: () {}, child: const Text('Blog')),
+                    ]),
+                Wrap(
+                    crossAxisAlignment: WrapCrossAlignment.center,
+                    direction: Axis.horizontal,
+                    spacing: 16,
+                    children: [
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Remix.github_fill)),
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Remix.medium_fill)),
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Remix.behance_fill)),
+                    ]),
+              ]);
         }),
       ),
     );
--- lib/src/core/widgets/mobile/mbl_navigation_bar.dart
+++ lib/src/core/widgets/mobile/mbl_navigation_bar.dart
@@ -1,7 +1,6 @@
-import 'package:emanoelviana/src/core/extensions/theme_extension.dart';
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
-import 'package:remixicon/remixicon.dart';
+
+import 'package:emanoelviana/src/core/extensions/theme_extension.dart';
 
 class MblNavigationBar extends StatelessWidget implements PreferredSizeWidget {
   const MblNavigationBar({super.key});
@@ -10,13 +9,7 @@ class MblNavigationBar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
       toolbarHeight: 64,
-      title: Text('Emanoel Viana', style: context.text.titleLarge),
-      actions: [
-        IconButton(onPressed: () {}, icon: const Icon(Remix.github_fill)),
-        const Gap(8),
-        IconButton(onPressed: () {}, icon: const Icon(Remix.medium_fill)),
-        const Gap(16),
-      ],
+      title: Text('Emanoel Viana', style: context.text.titleMedium),
     );
   }
 
